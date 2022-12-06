f3cf34c0c90a1572249d2210f68f3186f51e23c7
natecybersec <114831682+natecybersec@users.noreply.github.com>
2022-12-06 10:28:40 -0500

    Add files via upload

---
+++ E-Portfolio_2.0/dropdown.js
@@ -0,0 +1,36 @@
+// Navbar button
+function showDropdown() {
+    document.getElementById("Dropdown").classList.toggle("show");
+}
+
+ // Journals drop down button
+function showJournals() {
+    var journalContent = document.getElementsByClassName("journal-dropdown");
+    for (i = 0; i < journalContent.length; i++) {
+        var journal = journalContent[i];
+        journal.classList.toggle("show");
+    }
+}
+
+// Articles drop down button
+function showArticles() {
+    var articleContent = document.getElementsByClassName("article-dropdown");
+    for (i = 0; i < articleContent.length; i++) {
+        var article = articleContent[i];
+        article.classList.toggle("show");
+    }
+}
+
+// Exclusive to navbar for right now - if clicked outside navbar dropdown button, hide.
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdownContent = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdownContent.length; i++) {
+            var openDropdown = dropdownContent[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
